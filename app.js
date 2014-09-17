7fa5a4cd1c1ecce8f5b2e94d8a627224ded640dd
hydRAnger <armyiljfe@gmail.com>
2014-09-17 19:18:52 +0800

    just for backup

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
+var Movie = require('./models/movie.js')
 var port = process.env.PORT || 3000
 var app = express()
 var bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://localhost/HMDB')
 
 app.set('views', './views/pages')
 app.set('view engine', 'jade')//default template engine
@@ -14,38 +19,27 @@ console.log('HMDB start on port:' + port)
 
 //index page
 app.get('/', function(req, res){
-  res.render('index', {
-    title : 'HMDB homepage',
-    movies : [
-      {
-        title : 'Bat Man',
-        _id : 1,
-        poster : 'http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX640_SY720_.jpg'
-      },
-      {
-        title : 'Matrix',
-        _id : 2,
-        poster : 'http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX640_SY720_.jpg'
-      }
-    ]
+  Movie.fetch(function(err, movies) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.render('index', {
+      title : 'HMDB homepage',
+      movies : movies
+    })
   })
 })
 
 //detail page
 app.get('/movie/:id', function(req, res){
-  res.render('detail', {
-    title : 'HMDB detail page',
-    movie : {
-      director : "Norlan",
-      nation : "US",
-      title : "Bat Man",
-      year : 2008,
-      poster : 'http://player.youku.com/player.php/sid/XMzE2ODM0Njk2/v.swf',
-      language : "English",
-      summary : "从亲眼目睹父母被人杀死的阴影中走出来的“蝙蝠侠”，经历了成长之后，已经不再是那个桀骜不的孤单英雄了。在警官吉姆·戈登和检查官哈维·登特的通力帮助下，“蝙蝠侠”无后顾之忧地继续满世界的奔波，与日益增长起来的犯罪威胁做着永无休止的争斗，而他所在的高谭市，也是进展最为明显的地方，犯罪率以一种惊人的速度持续下降着，毕竟对方是能够上天入地的“蝙蝠侠”，不借两个胆子谁还敢造次呢？不过像高谭这种科技与污秽并存的城市，平静是不可能维持太久的，果不其然，新一轮的混乱很快就席卷了整个城市，人们再一被被恐慌所笼罩着，而声称愿意为这一切负责的，自然就是所有混乱的源头以及支配者--“小丑”了。 ",
-      flash : "http://v.youku.com/v_show/id_XMzE2ODM0Njk2.html"
+  var id = req.params.id
 
-    }
+  Movie.findById(id, function(err, movie) {
+    res.render('detail', {
+      title : 'HMDB' + movie.title,
+      movie : movie
+    })
   })
 })
 
@@ -66,23 +60,73 @@ app.get('/admin/movie', function(req, res){
   })
 })
 
+// admin update movie
+app.get('/admin/update/:id', function(req, res){
+  var id = req.params.id
+
+  if (id) {
+    Movie.findById(id, function(err, movie) {
+      res.render('admin', {
+        title : "Update movie",
+        movie : movie
+      })
+    })
+  }
+})
+
+//admin post movie
+app.post('/admin/movie/new', function(req, res) {
+  var id = req.body.movie._id
+  var movieObj = req.body.movie
+  var _movie
+
+  if ( _id !== 'undefined' ) {
+    Movie.findById(id, function(err, movie) {
+      if (err) {
+        console.log(err)
+      }
+
+      _movie = _.extend(movie, movieObj)
+      _movie.save(function(err, movie) {
+        if (err) {
+          console.log(err)
+        }
+
+        res.redirect('/movie/' + movie._id)
+      })
+    })
+  } else {
+    _movie = new Movie({
+      director : movieObj.director,
+      title : movieObj.title,
+      nation : movieObj.nation,
+      language : movieObj.language,
+      year : movieObj.year,
+      poster : movieObj.poster,
+      summary : movieObj.summary,
+      flash : movieObj.flash
+    })
+
+    _movie.save(function(err, movie) {
+      if (err) {
+        console.log(err)
+      }
+      res.redirect('/movie/' + movie._id)
+    })
+  }
+})
+
 //list page
 app.get('/admin/list', function(req, res){
-  res.render('list', {
-    title : 'HMDB list page',
-    movies : [
-      {
-        title : 'Bat Man',
-        _id : 1,
-        director : "Norlan",
-        nation : "US",
-        title : "Bat Man",
-        year : 2008,
-        language : "English",
-        flash : "http://v.youku.com/v_show/id_XMzE2ODM0Njk2.html"
-      }
-    ]
+  Movie.fetch(function(err, movies) {
+    if (err) {
+      console.log(err)
+    }
 
+    res.render('list', {
+      title : 'HMDB list',
+      movies : movies
+    })
   })
 })
 
